2fb13bea817b83831b40c0695bda813583c69059
Ruby <31341015+r0963033043@users.noreply.github.com>
2022-03-25 23:53:30 +0800

    Create LRUCache.cpp

---
+++ test/LRUCache.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string LRUCache(string strArr[], int arrLength) {
+  
+  // code goes here  
+  return strArr[0];
+
+}
+
+int main(void) { 
+   
+  // keep this function call here
+  string A[] = coderbyteInternalStdinFunction(stdin);
+  int arrLength = sizeof(A) / sizeof(*A);
+  cout << LRUCache(A, arrLength);
+  return 0;
+    
+}
