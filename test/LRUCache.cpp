c5bf7d0f55a235c60c5164e9fe685b24bbf0ca0b
Ruby <31341015+r0963033043@users.noreply.github.com>
2022-04-11 12:07:30 +0800

    Update LRUCache.cpp

--- test/LRUCache.cpp
+++ test/LRUCache.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>   // std::find
 using namespace std;
 
 string LRUCache(string strArr[], int arrLength) {
-  
   // code goes here  
+
+  string *str;
+  vector<string> ret;
+  int len = 0;
+
+  for(int i=0; i<arrLength; i++){
+    str = &(strArr[i]);
+    if(find(ret.begin(), ret.end(), *str) != ret.end()){
+      // move previous one to back
+      // erase finding one, than add to the back
+      ret.erase(find(ret.begin(), ret.end(), *str));
+      ret.push_back(*str);
+    } else{
+      // add first appear char
+      ret.push_back(*str);
+      len++;
+    }
+  }
+
+  strArr[0] = "";
+  for(int i=len-5; i<len-1; i++){
+    if(ret[i] != "")
+      strArr[0] += ret[i] + ", ";
+  }
+  strArr[0] += ret[len-1];
+
   return strArr[0];
 
 }
@@ -12,9 +39,19 @@ string LRUCache(string strArr[], int arrLength) {
 int main(void) { 
    
   // keep this function call here
-  string A[] = coderbyteInternalStdinFunction(stdin);
+//  string A[] = coderbyteInternalStdinFunction(stdin);
+  string A[] = {"A", "B", "A", "C", "A", "B"}; //  C-A-E-D-Z
+
+  /*
+   * Input: {"A", "B", "A", "C", "A", "B"}
+     Output: C-A-B
+     Input: {"A", "B", "C", "D", "E", "D", "Q", "Z", "C"}
+     Output: E-D-Q-Z-C
+   */
+
   int arrLength = sizeof(A) / sizeof(*A);
   cout << LRUCache(A, arrLength);
+  cout << "\n";
   return 0;
     
 }
